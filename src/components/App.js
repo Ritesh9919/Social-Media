84fa325a11919af95eb926b87f44eb7179cfb35a
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-21 13:55:26 +0530

    checked customfetch function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+import {getPosts} from '../api';
 
 
 function App() {
+  useEffect(()=> {
+    async function fetchPosts() {
+     const response = await getPosts();
+     console.log(response);
+    }
+    fetchPosts();
+  },[])
   return (
     <div className="App">
       <h1>Hello World</h1>
