7f9245e85ce93654e5d9b6cb906db4c0e2a0ce15
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-25 10:14:45 +0530

    used react-toastify for sending notifications

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import {App} from './components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -9,6 +11,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
   </React.StrictMode>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,53 @@
+import { ToastContainer, toast } from 'react-toastify';
+import { useState } from 'react';
 import styles from '../styles/login.module.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
+    if (!email || !password) {
+      return toast.error('Please enter both email and password', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    }
+  };
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" required />
+        <input
+          type="password"
+          placeholder="Paasword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <div className={styles.field}>
-        <button>Log In</button>
+      <div className={styles.field} aria-disabled={loggingIn}>
+        <button>{loggingIn ? 'Logging In...' : 'Log In'}</button>
       </div>
     </form>
   );
