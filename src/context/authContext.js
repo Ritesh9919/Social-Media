a207fbc355a97aaa81d10198657cdeaf450a90b5
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-30 15:36:41 +0530

    created userProvideAuth custom hook

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,5 @@
 import {createContext} from 'react';
+import { usePrivideAuth } from '../hooks/useProvideAuth';
 
 const initialValue = {
     user:null,
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import {App} from './components';
+import { App } from './components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
+import AuthProvider from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
     <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
   </React.StrictMode>
 );
-
-
