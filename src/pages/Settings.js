31cca09831d8234b4671671fc1324fdfe145b73f
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-11-03 11:26:36 +0530

    implemented the setting logic in setting UI

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,37 +1,55 @@
+import { useState } from 'react';
+import { useAuth } from '../hooks/useProvideAuth';
 import styles from '../styles/settings.module.css';
 
 function Settings() {
- return (
+  const auth = useAuth();
+  const [editMode, setEditMode] = useState(false);
+  return (
     <div className={styles.settings}>
-        <div className={styles.avtarImg}>
-            <img src="https://tse1.mm.bing.net/th?id=OIP.nczpMSa69aDJWYGi0tKqggHaHa&pid=Api&P=0&h=180"/>
-        </div>
+      <div className={styles.avtarImg}>
+        <img src="https://tse1.mm.bing.net/th?id=OIP.nczpMSa69aDJWYGi0tKqggHaHa&pid=Api&P=0&h=180" />
+      </div>
       <div className={styles.fields}>
         <div className={styles.fieldLebel}>Email</div>
-        <div className={styles.fieldValue}>Email</div>
+        <div className={styles.fieldValue}>{auth.user?.email}</div>
       </div>
 
-      <div className={styles.fields}>
-        <div className={styles.fieldLebel}>Name</div>
-        <div className={styles.fieldValue}>Ritesh</div>
-      </div>
+      {editMode ? (
+        <div className={styles.fields}>
+          <div className={styles.fieldLebel}>Name</div>
+          <input type="text" placeholder="Name" />
+        </div>
+      ) : (
+        <div className={styles.fields}>
+          <div className={styles.fieldLebel}>Name</div>
+          <div className={styles.fieldValue}>{auth.user?.name}</div>
+        </div>
+      )}
 
       <div className={styles.fields}>
         <div className={styles.fieldLebel}>Password</div>
-        <input type="password" placeholder='Password'/>
+        <input type="password" placeholder="Password" />
       </div>
 
       <div className={styles.fields}>
         <div className={styles.fieldLebel}>Confirm Password</div>
-        <input type="password" placeholder='Confirm Password'/> 
+        <input type="password" placeholder="Confirm Password" />
       </div>
 
       <div className={styles.fields}>
-        <button>Edit Profile</button>
+        {editMode ?(
+          <div className={styles.btnGroup}>
+          <button>Save</button>
+          <button onClick={()=> setEditMode(false)}>Go Back</button>
+          </div>
+        ):(
+          <button onClick={() => setEditMode(true)}>Edit Profile</button>
+        )}
+        
       </div>
     </div>
- )
+  );
 }
-
 
 export default Settings;
